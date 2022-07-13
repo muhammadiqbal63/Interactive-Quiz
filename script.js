e7cfc09488552df626c0662fbef68adc8b0fe470
muhammadiqbal63 <muhammadiqbalid63@gmail.com>
2022-07-13 13:10:32 +0700

    part 2

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let score = 0;
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+
+  //check answer
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score = score + 25;
+    }
+  });
+  console.log(score);
+});
